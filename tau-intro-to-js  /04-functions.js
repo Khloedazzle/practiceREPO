725c1d5564262c617b27d8f571ebe42187f69f3c
khloedazzle <khloedazzle@gmail.com>
2023-03-25 15:12:54 -0500

    practiced different kinds of functions

---
+++ tau-intro-to-js  /04-functions.js
@@ -0,0 +1,14 @@
+function sum(a,b){
+    return a+b;
+
+}
+console.log(sum(10,10));
+
+function product(a,b){
+    return a*b;
+
+}
+console.log(product(123,123));
+
+const bigNumbers = product(12333,666778);
+console.log(bigNumbers);
